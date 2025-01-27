db6474cd052e8dc9e19bd56c883efaa2f29e8729
Tolik <tolik@svl.services>
2025-01-26 22:42:46 -0800

    added function genkit flow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,10 +10,24 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
+// Import the flow from genkit-sample.ts
+import {menuSuggestionFlow} from "./genkit-sample";
+
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+export const generateMenu = onRequest(async (request, response) => {
+  try {
+    // Call the flow with a theme (using "italian" as an example)
+    const result = await menuSuggestionFlow({theme: "italian"});
+    response.send(result);
+  } catch (error) {
+    logger.error("Error generating menu", error);
+    response.status(500).send("Error generating menu suggestion");
+  }
 });
